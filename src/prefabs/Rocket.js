abfdddb96ab6aa091e8518b3def900878722f741
Gabriel Gallegos <gabesoccer66@gmail.com>
2021-04-23 18:52:28 -0700

    movement

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -6,7 +6,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
       // add object to existing scene
       scene.add.existing(this);
       this.isFiring = false;
-      this.moveSpeed = 2;
+      this.moveSpeed = 6;
       this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
       this.sfxmovement = scene.sound.add('sfx_select');
     }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -33,7 +33,7 @@ class Play extends Phaser.Scene {
     this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
     // add spaceships (x3)
     this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
-    this.ship04 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'super', 0, 35).setOrigin(0, 0);
+    this.ship04 = new Spaceship(this, game.config.width + borderUISize*2, borderUISize*100, 'super', 0, 35).setOrigin(0, 0);
     this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
     this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
     // define keys
